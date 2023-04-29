ce4237b64961894b84a4bdbe9d06d313cdf17f5d
Tommy Le <tommyle@csu.fullerton.edu>
2023-04-28 22:24:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import calendar
 
-print("Check your month according to the year")
-#year, month
-yy= 2023
-mm = 2
+print("Check your month according to the year: ")
+# ask for year input
+yy = int(input("Enter the year (yyyy): "))
 
-#displays calendar
+# ask for month input
+mm = int(input("Enter the month (1-12): "))
+
+# displays calendar
 print(calendar.month(yy, mm))
