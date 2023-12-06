5189b32f91729d5f101e5a1ca5e90ca732982d5c
Tommy Le <tommyle@csu.fullerton.edu>
2023-12-06 12:55:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import calendar
 
 print("Check your month according to the year: ")
-# ask for year input
+
+# ask for year input and converts it to an integer
 yy = int(input("Enter the year (yyyy): "))
 
-# ask for month input
+# ask for month input and converts it into a integer
 mm = int(input("Enter the month (1-12): "))
 
 # displays calendar
